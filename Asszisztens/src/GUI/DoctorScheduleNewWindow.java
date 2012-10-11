96a4034bd382242b94baf2dbae1fee7b456628fd
László Dobó <dobo.laszlou@gmail.com>
2012-10-11 02:04:45 +0200

    remove warnings

--- Asszisztens/src/GUI/DoctorScheduleNewWindow.java
+++ Asszisztens/src/GUI/DoctorScheduleNewWindow.java
@@ -9,7 +9,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
-import java.sql.SQLException;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Date;
